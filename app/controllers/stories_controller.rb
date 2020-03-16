1a490307ec6f8071b9b32894233b6eac96831230
Paco <paco15241@gmail.com>
2020-03-16 23:50:25 +0800

    finish publish and unpublish feature - 1.10

---
+++ app/controllers/stories_controller.rb
@@ -0,0 +1,62 @@
+class StoriesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :find_story, only: [:edit, :update, :destroy]
+
+  def index
+    @stories = current_user.stories.where(deleted_at: nil).order(created_at: :desc)
+  end
+
+  def new
+    @story = current_user.stories.new
+  end
+
+  def create
+    @story = current_user.stories.new(story_params)
+    @story.status = 'published' if params[:publish]
+
+    if @story.save
+      if params[:publish]
+        redirect_to stories_path, notice: '已成功發佈故事'
+      else
+        redirect_to edit_story_path(@story), notice: '故事已儲存'
+      end
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @story.update(story_params)
+      case
+      when params[:publish]
+        @story.publish!
+        redirect_to stories_path, notice: '故事已發佈'
+      when params[:unpublish]
+        @story.unpublish!
+        redirect_to stories_path, notice: '故事已下架'
+      else
+        redirect_to edit_story_path(@story), notice: '故事已儲存'
+      end
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @story.destroy
+    redirect_to stories_path, notice: '故事已刪除'
+  end
+
+  private
+  def find_story
+    @story = current_user.stories.find(params[:id])
+  end
+
+  def story_params
+    params.require(:story).permit(:title, :content)
+  end
+
+end
