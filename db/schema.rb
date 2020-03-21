26c5f77b681b9bff327bb6cc03b2e22989123037
Paco <paco15241@gmail.com>
2020-03-21 22:27:59 +0800

    add username and intro to user model - 1.12

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_19_091036) do
+ActiveRecord::Schema.define(version: 2020_03_20_145835) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,8 +48,11 @@ ActiveRecord::Schema.define(version: 2020_03_19_091036) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
+    t.text "intro"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "stories", "users"
