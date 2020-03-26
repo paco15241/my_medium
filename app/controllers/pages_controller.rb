a30de426a377df48075a1d2119e80989af91363b
Paco <paco15241@gmail.com>
2020-03-26 16:48:21 +0800

    show story content page - 1.20

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
-
+  before_action :find_story, only: [:show]
+  
   def index
     # @stories = Story.order(created_at: :desc).includes(:user)
     # @stories = Story.where(status: 'published').order(created_at: :desc).includes(:user)
@@ -12,6 +13,11 @@ class PagesController < ApplicationController
   end
 
   def user
+  end
+
+  private
+  def find_story
+    @story = Story.friendly.find(params[:story_id])
   end
 
 end
