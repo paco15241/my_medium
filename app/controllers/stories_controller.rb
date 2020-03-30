784c472c2af05a058dcc6723fd000977f0cbc29f
Paco <paco15241@gmail.com>
2020-03-30 23:09:46 +0800

    [feature] clap - part 2 - 1.25

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,6 +1,7 @@
 class StoriesController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:clap]
   before_action :find_story, only: [:edit, :update, :destroy]
+  skip_before_action :verify_authenticity_token, only: [:clap]
 
   def index
     @stories = current_user.stories.where(deleted_at: nil).order(created_at: :desc)
@@ -51,6 +52,16 @@ class StoriesController < ApplicationController
     redirect_to stories_path, notice: '故事已刪除'
   end
 
+  def clap
+    if user_signed_in?
+      story = Story.friendly.find(params[:id])
+      story.increment!(:clap)
+      render json: {status: story.clap}
+    else
+      render json: {status: 'sign_in_first'}
+    end
+  end
+  
   private
   def find_story
     @story = current_user.stories.friendly.find(params[:id])
